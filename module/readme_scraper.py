af5d181dcafa9b4201db5e001e0e6975691858b8
Nick Perugini <nickperugini10@gmail.com>
2025-05-23 12:35:16 -0400

    Delete module directory

--- module/readme_scraper.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def scrape_github_readme(repo_url: str) -> str:
-    if not repo_url.endswith("/"):
-        repo_url += "/"
-    raw_url = repo_url.replace("github.com", "raw.githubusercontent.com") + "main/README.md"
-    response = requests.get(raw_url)
-    return response.text if response.status_code == 200 else "README not found."
